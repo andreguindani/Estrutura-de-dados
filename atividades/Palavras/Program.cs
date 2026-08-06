bfda3e4deafb08504474aa2211f894a2215e7d06
André <aguindani7@gmail.com>
2026-08-05 22:14:32 -0300

    aula02

---
+++ atividades/Palavras/Program.cs
@@ -0,0 +1,27 @@
+﻿string firstName = "Andre";
+string lastName = "Guindani";
+
+string note = lastName.ToUpper() + " " + firstName;
+
+string initials = lastName[0] + " " + firstName[0];
+
+Console.WriteLine("" + initials);
+
+//formatação de string
+
+string texto = string.Format(
+    "{0} {1} nascido em {2}", 
+    firstName, 
+    lastName, 
+    "2007"
+);
+
+Console.WriteLine(texto);
+
+//C# é uma linguagem filha do C++
+//Totalmente orientada a objetos, portanto tudo dentro do C é descendente do tipo object
+
+int age = 24;
+object ageBoxing = age;
+int ageUnboxing = (int)ageBoxing;
+
