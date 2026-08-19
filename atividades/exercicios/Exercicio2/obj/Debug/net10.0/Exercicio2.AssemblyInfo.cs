0bb674b27862bb40292bdce54ae1170febb5d66b
André Guindani <aguindani7@gmail.com>
2026-08-19 20:39:07 -0300

    atv1

--- atividades/exercicios/Exercicio2/obj/Debug/net10.0/Exercicio2.AssemblyInfo.cs
+++ atividades/exercicios/Exercicio2/obj/Debug/net10.0/Exercicio2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad4df50e11d77e63a0319790cef51aba4c258687")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+003adb0e350d1bd18e6486ad2f121ce179073769")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ atividades/matrizes/matrizes/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Vetores Multi-Dimensionais */
+
+//Vetor Bi-Dimensional
+int[, ] numberBi = new int[5,2];
+
+//Vetor Tri-Dimensional
+int[,,] numbersTri = new int[5,4,3];
+
+//Podemos combinar a declaração com a inicialização
+//Exemplo de um 4x3
+int[,] newNumbersBi = new int[,]{
+    {9,5,-9},
+    {-11,3,0},
+    {6,115,3},
+    {-12,-9,71},
+    {1,-6,-1}
+}
+
+//Percorendo a Matriz
+//Exemplo de repetição for encadeada
+//Nested/Chained For
+
+for (int i = 0; i < newNumbersBi.)
+{
+    
+}
