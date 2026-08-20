38f073dbadcd9de9c6b8321bcf76304c3586774d
André Guindani <aguindani7@gmail.com>
2026-08-19 22:06:54 -0300

    listas

---
+++ atividades/listas/Program.cs
@@ -0,0 +1,20 @@
+﻿//ArrayLists
+
+using System.Collections;
+
+ArrayList arrayList = new ArrayList();
+
+//adicionando itens a lista
+arrayList.Add(5);
+
+//adicionando quantias de itens a lista
+arrayList.AddRange(new int[]{1,2,3});
+
+arrayList.Insert(3,7.8);
+
+//percorrendo itens da lista de forma generica
+foreach (object obj in arrayList)
+{
+    Console.WriteLine(obj);
+}
+
